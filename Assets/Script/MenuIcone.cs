a92f9d57d9a176bc0d4365f57944ff1ed7391c54
Josef248 <giutut24@gmail.com>
2020-05-28 01:03:22 +0200

    Aggiunga menu Opzioni/Profilo

---
+++ Assets/Script/MenuIcone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void openMap()
+    {
+        SceneManager.LoadScene("scenaMappa");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
---
+++ Assets/Script/SalvaNome.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SalvaNome : MonoBehaviour
+{
+    public InputField textBox;
+
+    public void clickSaveButton()
+    {
+        PlayerPrefs.SetString("NamePlayer", textBox.text);
+        Debug.Log("Il nome del giocatore: " + PlayerPrefs.GetString("NamePlayer"));
+    }
+}
