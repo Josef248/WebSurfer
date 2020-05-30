ccfb14dd1b6132fc187e5a17c330b63a7407a0df
FraMarcuccio <francesco.marcuccio.fm@gmail.com>
2020-05-30 17:37:13 +0200

    FraPullPush1

---
+++ Assets/Script/CaricaScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CaricaScene : MonoBehaviour
+{
+    public void openMap()
+    {
+        SceneManager.LoadScene("Mappa");
+    }
+
+    public void scenaC2()
+    {
+        SceneManager.LoadScene("SpiegazioneConcetto2");
+    }
+
+
+}
