be5af6f9d22996af7821311cd97a96f46b51dcf8
Josef248 <giutut24@gmail.com>
2020-06-10 20:01:41 +0200

    Base funzionante

--- Assets/Script/Respawn.cs
+++ Assets/Script/Respawn.cs
@@ -8,69 +8,109 @@ public class Respawn : MonoBehaviour
 
     public GameObject prefab;
     private GameObject link;
+    public GameObject winner;
+
+    RectTransform misure;
+    float width, height;
+    
+    private Vector2 screenBounds;
+    private Vector2 whereToSpawn;
+    private Vector2[] otherPosition = new Vector2[35];
+    int numSpwan = 0;
 
     public float respawnTime = 0.3f;
-    private Vector2 screenBounds;
 
     public int numberLink = 2;
     public int stop = 9;
-
-    private int numLink_1;
+    private int numLink_1 = 1;
 
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
 
-        PlayerPrefs.SetInt("numeroLink",0);
+        misure = (RectTransform)prefab.transform;
+        width = misure.rect.width;
+        height = misure.rect.height;
+
+        PlayerPrefs.SetInt("numeroLink", numLink_1);
+
+        Debug.Log("scrB.x :" + screenBounds.x);
+        Debug.Log("scrB.y :" + screenBounds.y);
+
+        Debug.Log("w :" + Screen.width);
+        Debug.Log("h :" + Screen.height);
+
         StartCoroutine(linkWave(numberLink));
 
     }
 
+    Vector2 creaRandom()
+    {
+        Vector2 pos;
+        bool okei = false;
+
+        do
+        {
+            pos.x = Random.Range(-Screen.width, Screen.width)* 1 / 3;
+            pos.y = Random.Range(-Screen.height, Screen.height)* 1 / 3;
+
+            /*
+            pos.x = Random.Range(-500.0f, 500.0f);
+            pos.y = Random.Range(-500.0f, 500.0f);
+
+            pos.x = Random.Range(-screenBounds.x, screenBounds.x) * 2 / 3;
+            pos.y = Random.Range(-screenBounds.y, screenBounds.y) * 2 / 3;
+            */
+
+            if (numSpwan == 0)
+            {
+                otherPosition[numSpwan] = pos;
+                okei = true;
+
+            }
+            else
+            {
+                if (!esistePosition(otherPosition, pos))
+                {
+                    okei = true;
+                    otherPosition[numSpwan] = pos;
+
+                }
+            }
+
+
+        } while (okei == false);
+
+        numSpwan++;
+        return pos;
+    }
+
+    bool esistePosition(Vector2[] posizioni, Vector2 nuovoOggetto)
+    {
+        int i;
+
+        for (i = 0; i < posizioni.Length; i++)
+        {
+            if (Mathf.Abs(posizioni[i].x - nuovoOggetto.x) <= width && Mathf.Abs(posizioni[i].y - nuovoOggetto.y) <= height)
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+
     private void spawnLink()
     {
-        link = Instantiate(prefab) as GameObject;
+        whereToSpawn = creaRandom();
+
+        link = Instantiate(prefab, whereToSpawn, Quaternion.identity);
         link.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
         link.transform.SetParent(GameObject.FindGameObjectWithTag("Background").transform, false);
 
-        //do {
 
-            link.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x) * 2 / 3, Random.Range(-screenBounds.y, screenBounds.y) * 2 / 3);
-
-      // } while (Physics2D.OverlapBox(link.transform.position, 100f, 0f);
-        
-
-
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        Debug.Log("2 oggetti si sono collisi colEXIT");
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("2 oggetti si sono collisi triggerEXIT");
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        Debug.Log("2 oggetti si sono collisi ColisSTAY");
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("2 oggetti si sono collisi ColisENTER");
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("2 oggetti si sono collisi STAY");
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-        Debug.Log("2 oggetti si sono collisi");
     }
 
     IEnumerator linkWave(int volte)
@@ -90,17 +130,21 @@ public class Respawn : MonoBehaviour
     void Update()
     {
         numLink_1 = PlayerPrefs.GetInt("numeroLink");
-
+        
         if(numLink_1 == numberLink && numLink_1 <= stop)
         {
             numLink_1 = 0;
             PlayerPrefs.SetInt("numeroLink", numLink_1);
-
             numberLink += 2;
 
             StartCoroutine(linkWave(numberLink));
 
-            //Debug.Log("numLink_1 : " + numLink_1 + " , numberLink : " + numberLink);
+            Debug.Log("numLink_1 : " + numLink_1 + " , numberLink : " + numberLink);
+        }
+
+        if(numLink_1 > numberLink && numLink_1 > stop)
+        {
+            winner.SetActive(true);
         }
 
 
--- Assets/Script/ScaleTween.cs
+++ Assets/Script/ScaleTween.cs
@@ -6,14 +6,13 @@ using UnityEngine.UI;
 
 public class ScaleTween : MonoBehaviour
 {
-    //public Text punteggio;
     public UnityEvent onCompleteCallback;
     public int punteggioInt = 10;
 
     public void OnEnable()
     {
         transform.localScale = new Vector3(0, 0, 0);
-        LeanTween.scale(gameObject, new Vector3(1, 1, 1), 0.3f).setDelay(0.2f).setOnComplete(OnComplete);
+        LeanTween.scale(gameObject, new Vector3(1, 1, 1), 0.3f).setDelay(0.1f).setOnComplete(OnComplete);
     }
 
     public void OnComplete()
@@ -27,20 +26,17 @@ public class ScaleTween : MonoBehaviour
     public void OnClose()
     {
         SaliPunteggio();
-        //LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f).setOnComplete(DestroyMe);
-        LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f);
+        //LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f).setDelay(0.5f).setOnComplete(DestroyMe);
+        LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f).setOnComplete(DestroyMe);
+        //LeanTween.scale(gameObject, new Vector3(0, 0, 0), 0.5f);
         countLink();
 
     }
 
+
     public void DestroyMe()
     {
         Destroy(gameObject);
-    }
-
-    public void Invisible()
-    {
-        gameObject.GetComponent<Renderer>().enabled = false;
     }
 
     public void countLink()
@@ -49,7 +45,6 @@ public class ScaleTween : MonoBehaviour
         linkOra++;
         PlayerPrefs.SetInt("numeroLink", linkOra);
 
-        ///Debug.Log("Numero link chiusi : " + linkOra);
     }
 
 
@@ -57,9 +52,7 @@ public class ScaleTween : MonoBehaviour
     {
         int puntiOra = PlayerPrefs.GetInt("HighScore");
         PlayerPrefs.SetInt("HighScore", punteggioInt + puntiOra);
-        //punteggio.text = PlayerPrefs.GetInt("HighScore").ToString();
 
-        //Debug.Log("X premuta, punteggio totale = " + (punteggioInt + puntiOra));
     }
 
 
