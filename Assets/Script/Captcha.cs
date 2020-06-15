8a800d157c95604e723bfb66b84982a21bd1d3c1
Giuseppe <giuseppezeta@gmail.com>
2020-06-15 12:55:25 +0200

    lv4G

---
+++ Assets/Script/Captcha.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Captcha : MonoBehaviour
+{
+    public GameObject[] lettere;
+    public GameObject[] lettere1;
+    public GameObject[] lettere2;
+    public GameObject l;
+    GameObject l2;
+    //public TextMeshProUGUI l;
+
+    void Start()
+    {
+        captchaGen();
+        captchaGen2();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void captchaGen()
+    {
+        float x = -3f;
+        int c;
+        int t;
+        Vector2 coord = new Vector2(x, 2);
+        for (int i = 0; i < 4; i++)
+        {
+            c = Random.Range(0,20);
+            x++;
+            coord = new Vector2(x, 2);
+            t = Random.Range(0, 3);
+            t = 0;
+            switch (t)
+            {
+                case 0:
+                    Instantiate(lettere[c], coord, Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(lettere1[c], coord, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(lettere2[c], coord, Quaternion.identity);
+                    break;
+            }
+        }
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX;
+        float randomY;
+        randomX = 589f;
+        randomY = Random.Range(-884f, -559f);
+        return new Vector2(randomX, randomY);
+    }
+
+    void captchaGen2()
+    {
+        char[] lett = {'a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        l.GetComponent<TextMeshProUGUI>().text = lett[Random.Range(0, 23)].ToString();
+        //Instantiate(l, GetRandomPosition(), Quaternion.identity);
+        l2=Instantiate(l, new Vector2(2.35f, -2.2f), Quaternion.identity);
+        l2.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+        l2.transform.localScale = new Vector2(1,1);
+    }
+
+}
---
+++ Assets/Script/moveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(3.36f, -2.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPosition = new Vector2(-4.19f, -2.2f);
+        if ((Vector2)transform.position != targetPosition)
+        {   
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, 1 * Time.deltaTime);
+        }
+}
+}
