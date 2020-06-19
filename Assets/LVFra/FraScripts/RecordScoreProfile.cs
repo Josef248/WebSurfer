331feb6360017b1fe7643032f89ec7985f91b370
FraMarcuccio <francesco.marcuccio.fm@gmail.com>
2020-06-19 12:07:35 +0200

    Profilo e Credits
    Aggiornato profilo aggiungendo record lv e aggiounto pulsante e pagina credits

---
+++ Assets/LVFra/FraScripts/RecordScoreProfile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RecordScoreProfile : MonoBehaviour
+{
+    public TextMeshProUGUI Score1;
+    //public TextMeshProUGUI Highscore;
+    public int numeroLivello;
+
+    void Start()
+    {
+        //score.text = PlayerPrefs.GetInt("Score" + numeroLivello).ToString();
+        Score1.text = PlayerPrefs.GetInt("Highscore" + numeroLivello).ToString();
+    }
+}
