7e60b022b20333bc2c09a82eb8b68c4938f34c72
Josef248 <giutut24@gmail.com>
2020-06-22 16:31:06 +0200

    fix FINALI

---
+++ Assets/Script/CaricaNome.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CaricaNome : MonoBehaviour
+{
+    public TextMeshProUGUI nameBox;
+
+    void Start()
+    {
+        nameBox.text = PlayerPrefs.GetString("NamePlayer");
+    }
+
+    void Update()
+    {
+        nameBox.text = PlayerPrefs.GetString("NamePlayer");
+    }
+
+
+}
